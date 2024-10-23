543466c30e5e7d85f4e362acecb8509527997f51
Yicheng Wang <yichengw1011@gmail.com>
2024-10-23 10:31:00 -0700

    make sidebar hiddable

--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -6,34 +6,53 @@ import { ModalProvider, Modal } from "../context/Modal";
 import { thunkAuthenticate } from "../redux/session";
 import Navigation from "../components/Navigation/Navigation";
 import SideBar from "../components/SideBar/SideBar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Layout() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   const sessionUser = useSelector(state => state.session.user);
-  const location = useLocation()
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
 
   useEffect(() => {
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible)
+  }
 
   return (
     <>
       <ModalProvider>
         <div className='layout'>
           <Navigation />
+
           <div className='content-container'>
+
             {sessionUser && (
-              <div className='side-bar'>
-                <SideBar />
-              </div>
+              <>
+                <div className={`side-bar ${isSidebarVisible ? '' : 'hidden'}`}>
+                  <SideBar />
+                </div>
+
+                <div onClick={toggleSidebar} className={`toggle-sidebar-btn ${isSidebarVisible ? '' : 'outside'}`}>
+                  <FontAwesomeIcon icon={isSidebarVisible ? faChevronLeft : faChevronRight} />
+                </div>
+              </>
+              
             )}
 
             <div className={`main-content ${ !sessionUser ? 'center-content' : ''}`}>
               {isLoaded && <Outlet />}
             </div>
+
             <Modal />
+
           </div>
+
         </div>
       </ModalProvider>
     </>
